b866797c7882cafbd826e468de8f34684c324f9c
bisma <semesterempat95@gmail.com>
2025-03-04 21:46:19 +0800

    Keterangan commit

---
+++ pertemuan 01/src/pertemuan/pkg01/Pertemuan01.java
@@ -0,0 +1,16 @@
+package pertemuan.pkg01;
+
+/**
+ *
+ * @author LAB F
+ */
+public class Pertemuan01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print ("Belajar Bahasa Java")
+    }
+    
+}
